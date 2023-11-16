0b4b2fc5c23f282957aeefbc61c5728cc4f85950
abdelhadi-22 <abdoelbcir33@gmail.com>
2023-11-16 15:36:07 +0100

    first commit

---
+++ src/pages/Login.js
@@ -0,0 +1,54 @@
+// Import necessary libraries
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    const apiUrl = 'https://your-api-endpoint.com/login';
+
+    try {
+      const response = await axios.post(apiUrl, { username, password });
+      console.log('Login successful', response.data);
+      setUsername('');
+      setPassword('');
+    } catch (error) {
+      console.error('Login failed', error.message);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <label>
+          Username:
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </label>
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+
+export default Login;
